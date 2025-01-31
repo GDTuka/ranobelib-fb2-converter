7157b4812a97de7983b1205fb88792308a6ae295
artem soglaev <artem.soglaev@mail.ru>
2025-01-31 13:31:50 +0300

    fixes, now to find header go try to find any h1

--- main.go
+++ main.go
@@ -81,23 +81,41 @@ func downloadImage(url string) (FB2Image, error) {
 }
 
 func main() {
-	handleDownloadAll()
+	handleDownloadAll(true)
 }
 
-func handleDownloadAll() {
-	if len(os.Args) < 2 {
-		fmt.Println("No JSON parameter provided")
-		return
-	}
-
-	jsonParam := os.Args[1]
-
+func handleDownloadAll(useConfig bool) {
+	var jsonParam string
 	var config Config
-	err := json.Unmarshal([]byte(jsonParam), &config)
-	if err != nil {
-		fmt.Println("Error parsing JSON:", err)
-		return
-	}
+	if useConfig {
+
+		file, err := os.ReadFile("config.json")
+		if err != nil {
+			fmt.Println("Error reading config file:", err)
+			os.Exit(1)
+		}
+		err = json.Unmarshal(file, &config)
+		if err != nil {
+			fmt.Println("Error parsing JSON:", err)
+			return
+		}
+
+			fmt.Println("Using config")
+		} else {
+
+			if len(os.Args) < 2 {
+				fmt.Println("No JSON parameter provided")
+				return
+			}
+		
+			jsonParam = os.Args[1]
+			err := json.Unmarshal([]byte(jsonParam), &config)
+			if err != nil {
+				fmt.Println("Error parsing JSON:", err)
+				return
+			}
+		
+		}
 
 	uiRequestUrl := config.Url
 	requestUrl := config.ApiUrl
@@ -179,14 +197,14 @@ func handleDownloadAll() {
 			continue
 		}
 
-		headerRe := regexp.MustCompile(`<h1\s+class="k6_cr"[^>]*>([\s\S]*?)</h1>`)
+		headerRe := regexp.MustCompile(`<h1[^>]*>([\s\S]*?)</h1>`)
 		headerMathes := headerRe.FindStringSubmatch(html)
 
 		if len(headerMathes) > 1 {
+			fmt.Println("H1 found %s", strings.TrimSpace(headerMathes[0]))
 			contentString += fmt.Sprintf("%s\n\n", strings.TrimSpace(headerMathes[0]))
 		} else {
-			fmt.Println("H1 with class 'k6_cr' not found")
-			fmt.Println(html)
+			fmt.Println("H1 not found")
 		}
 
 		// Find the div with class "text-content"
@@ -252,7 +270,6 @@ func handleDownloadAll() {
 			}
 		} else {
 			fmt.Println("Div with class 'text-content' not found")
-			fmt.Println(html)
 		}
 
 		time.Sleep(1 * time.Second)
@@ -343,152 +360,3 @@ func convertHTMLToFB2(htmlContent string, images []FB2Image) (string, error) {
 	fb2.WriteString("</FictionBook>")
 	return fb2.String(), nil
 }
-
-// func singleHandle() {
-
-// 	if len(os.Args) < 2 {
-// 		fmt.Println("No JSON parameter provided")
-// 		return
-// 	}
-
-// 	jsonParam := os.Args[1]
-
-// 	var config Config
-// 	err := json.Unmarshal([]byte(jsonParam), &config)
-// 	if err != nil {
-// 		fmt.Println("Error parsing JSON:", err)
-// 		return
-// 	}
-
-// 	var contentString string = ""
-// 	var allImages []FB2Image
-
-// 	ctx, cancel := context.WithTimeout(context.Background(), 60*30*time.Second)
-// 	defer cancel()
-
-// 	// Create Chrome instance
-// 	opts := append(chromedp.DefaultExecAllocatorOptions[:],
-// 		chromedp.Flag("disable-web-security", true),
-// 		chromedp.Flag("no-sandbox", true),
-// 		chromedp.Flag("disable-setuid-sandbox", true),
-// 	)
-
-// 	allocCtx, cancel := chromedp.NewExecAllocator(ctx, opts...)
-// 	defer cancel()
-
-// 	ctx, cancel = chromedp.NewContext(allocCtx)
-// 	defer cancel()
-
-// 	modifiedUrl := "https://ranobelib.me/ru/154310--too-many-losing-heroines/read/v1/c0?ui=5260317"
-// 	fmt.Println(modifiedUrl)
-
-// 	var html string
-// 	err = chromedp.Run(ctx,
-// 		chromedp.Navigate(modifiedUrl),
-// 		// Wait for body to be present
-// 		chromedp.WaitReady("body", chromedp.ByQuery),
-// 		// Wait for text-content div to be present
-// 		chromedp.WaitVisible(".text-content", chromedp.ByQuery),
-// 		// Additional wait to ensure JavaScript loads
-// 		chromedp.Sleep(2*time.Second),
-// 		chromedp.OuterHTML("html", &html),
-// 	)
-
-// 	if err != nil {
-// 		log.Printf("Error processing URL %s: %v", modifiedUrl, err)
-
-// 	}
-
-// 	headerRe := regexp.MustCompile(`<h1\s+class="k6_cr"[^>]*>([\s\S]*?)</h1>`)
-// 	headerMathes := headerRe.FindStringSubmatch(html)
-
-// 	if len(headerMathes) > 1 {
-// 		contentString += fmt.Sprintf("%s\n\n", strings.TrimSpace(headerMathes[0]))
-// 	} else {
-// 		fmt.Println("H1 with class 'k6_cr' not found")
-// 		fmt.Println(html)
-// 	}
-
-// 	// Find the div with class "text-content"
-// 	re := regexp.MustCompile(`<div\s+class="text-content"[^>]*>([\s\S]*?)</div>`)
-// 	matches := re.FindStringSubmatch(html)
-
-// 	if len(matches) < 1 {
-// 		re := regexp.MustCompile(`<div\s+class="node-doc text-content"[^>]*>([\s\S]*?)</div>`)
-// 		matches = re.FindStringSubmatch(html)
-// 	}
-
-// 	if len(matches) > 1 {
-// 		divContent := matches[1]
-
-// 		// First, find and process images within the text-content div
-// 		imgRe := regexp.MustCompile(`<img[^>]+src="([^"]+)"[^>]*>`)
-// 		imgMatches := imgRe.FindAllStringSubmatch(divContent, -1)
-
-// 		if len(imgMatches) < 1 {
-
-// 			imgRe = regexp.MustCompile(`<img[^>]+class="_loaded node-image-item"[^>]+src="([^"]+)"[^>]*>`)
-// 			imgMatches = imgRe.FindAllStringSubmatch(divContent, -1)
-// 		}
-
-// 		// Create a map to store image replacements
-// 		imageReplacements := make(map[string]string)
-
-// 		for _, match := range imgMatches {
-// 			if len(match) > 1 {
-// 				imgURL := match[1]
-// 				if !strings.HasPrefix(imgURL, "http") {
-// 					// Handle relative URLs
-// 					if strings.HasPrefix(imgURL, "//") {
-// 						imgURL = "https:" + imgURL
-// 					} else {
-// 						imgURL = "https://" + strings.TrimPrefix(imgURL, "/")
-// 					}
-// 				}
-
-// 				img, err := downloadImage(imgURL)
-// 				if err != nil {
-// 					log.Printf("Error downloading image %s: %v", imgURL, err)
-// 					continue
-// 				}
-// 				allImages = append(allImages, img)
-
-// 				// Store the replacement for this image
-// 				imageReplacements[match[0]] = fmt.Sprintf(`<image l:href="#%s"/>`, img.ID)
-// 			}
-// 		}
-
-// 		// Replace images in the content with FB2 image references
-// 		for oldImg, newImg := range imageReplacements {
-// 			divContent = strings.Replace(divContent, oldImg, newImg, -1)
-// 		}
-
-// 		// Now process all elements including the replaced images
-// 		childRe := regexp.MustCompile(`<(\w+)[^>]*>([^<]*)</\w+>|<image[^>]+/>`)
-// 		childMatches := childRe.FindAllString(divContent, -1)
-
-// 		for _, child := range childMatches {
-// 			contentString += fmt.Sprintf("%s\n\n", strings.TrimSpace(child))
-// 		}
-// 	} else {
-// 		fmt.Println("Div with class 'text-content' not found")
-// 		fmt.Println(html)
-// 	}
-
-// 	contentFB2, err := convertHTMLToFB2(contentString, allImages)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	// Define the filename
-// 	filename := config.BookName + ".fb2"
-// 	path := filepath.Join(config.OutputDir, filename)
-
-// 	// Write the FB2 content to the file
-// 	err = os.WriteFile(path, []byte(contentFB2), 0644)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	fmt.Printf("FB2 file successfully created: %s\n", filename)
-// }
